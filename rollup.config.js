ac08a1dd53cb331af746fce5b3c59809cb1e1311
Daniel Rossi <electroteque@protonmail.com>
2023-06-09 04:32:28 +1000

    adding iife bundle

--- rollup.config.js
+++ rollup.config.js
@@ -50,5 +50,51 @@ export default [
 				file: 'build/three-webgpu.module.js'
 			}
 		]
+	},
+	{
+		input: './three-webgpu-renderer.js',
+		plugins: [
+		
+			includePaths({
+				paths: ["./src", "./three.js/examples/jsm/", "./three.js/examples/jsm/nodes", "./three.js/examples/jsm/renderers/webgpu"],
+				include: {
+				  'three': './three.js/build/three.module.js'
+				}
+		  	}),
+			strip({
+				debugger: true
+			}),
+			terser()
+		],
+		output: [
+			{
+				format: 'iife',
+				name: "THREE",
+				file: 'build/three-webgpu.min.js'
+			}
+		]
+	},
+	{
+		input: './three-webgpu-renderer.js',
+		plugins: [
+		
+			includePaths({
+				paths: ["./src", "./three.js/examples/jsm/", "./three.js/examples/jsm/nodes", "./three.js/examples/jsm/renderers/webgpu"],
+				include: {
+				  'three': './three.js/build/three.module.js'
+				}
+		  	}),
+			strip({
+				debugger: true
+			}),
+			terser()
+		],
+		output: [
+			{
+				format: 'iife',
+				name: "THREE",
+				file: 'build/three-webgpu.js'
+			}
+		]
 	}
 ];
