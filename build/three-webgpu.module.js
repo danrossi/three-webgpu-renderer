5c8b33c30f7984c3b29e8c520920cc760f1f92bc
Daniel Rossi <electroteque@protonmail.com>
2023-06-17 15:50:36 +1000

    add local video test

--- build/three-webgpu.module.js
+++ build/three-webgpu.module.js
@@ -51248,16 +51248,20 @@ class WebGPU {
 	static async isAvailable() {
 		if (navigator.gpu !== undefined) {
 
-			const adapter = await navigator.gpu.requestAdapter();
+			try {
+				const adapter = await navigator.gpu.requestAdapter();
 
-			if (adapter !== null) {
-				if (window.GPUShaderStage === undefined) {
+				if (adapter !== null) {
+					if (window.GPUShaderStage === undefined) {
 
-					window.GPUShaderStage = { VERTEX: 1, FRAGMENT: 2, COMPUTE: 4 };
+						window.GPUShaderStage = { VERTEX: 1, FRAGMENT: 2, COMPUTE: 4 };
 
+					}
+
+					return true;
 				}
-
-				return true;
+			} catch (e) {
+				return false;
 			}
 
 		}
